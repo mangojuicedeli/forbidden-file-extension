b187b03ecb2f78f00f8ff60a980541a934b0a74d
mangojuicedeli <mgkang0317@gmail.com>
2020-10-15 16:24:17 +0900

    initial commit

---
+++ src/main/java/com/flow/test/ForbiddenFileExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForbiddenFileExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForbiddenFileExtensionApplication.class, args);
+	}
+
+}
